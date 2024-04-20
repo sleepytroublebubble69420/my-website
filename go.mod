62312268cabf80f48942ec7742e52569f2dc43f3
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-20 15:10:43 +0200

    Moved static content inside dedicated folder and added http server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module my_website/http_server
+
+go 1.21.9
---
+++ http_server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "net/http"
+    "log"
+) 
+
+func main() {
+    fs_handler := http.FileServer(http.Dir("static/"))
+    http.Handle("/", fs_handler)
+
+    err := http.ListenAndServe(":1234", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
