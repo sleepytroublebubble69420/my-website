b1fef2f76bfc1034bd28ddc09d8c5dd5d59998f6
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-21 21:07:37 +0200

    Added an home page html file to start designing the home page

--- http_server/server.go
+++ http_server/server.go
@@ -18,10 +18,10 @@ func main() {
         log.Println(r.PostForm)
     })
 
-    fs_handler := http.FileServer(http.Dir("static_content/"))
+    fs_handler := http.FileServer(http.Dir("static_content"))
     http.Handle("/", fs_handler)
 
-    err := http.ListenAndServe(":1234", nil)
+    err := http.ListenAndServe(":8080", nil)
     if err != nil {
         log.Fatal(err)
     }
